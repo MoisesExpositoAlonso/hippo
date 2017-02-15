c9035be946ba0f3f97324b5e0a45ebe4366b712f
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2017-02-15 21:16:13 +0100

    removed many files. cleaning to get minimum scripts

--- jpg2png.py
+++
@@ -1,9 +0,0 @@
-import Image, sys
-
-mypicture=sys.argv[1]
-
-im = Image.open(mypicture)
-
-newname=mypicture[0:-3]+"png"
-im.save(newname)
-
--- mandartodoscripts.sh
+++
@@ -1,6 +0,0 @@
-!#/bin/bash
-
-git add *.py
-git commit -m $1
-git push origin master:remote
-git status
